30ba67de23f785353581eb36fd95bb083c7ea721
Chanda Gonet <chandagonet@gmail.com>
2023-06-09 13:23:57 -0400

    Improve accessability and performance

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const burgerIcon = document.getElementById('burger-icon')
 const menu = document.querySelector('nav')
 const links = document.querySelectorAll('li')
+const header = document.getElementById('header')
+const aboutMe = document.getElementById('about-me')
+
+header.addEventListener('click', () => {
+  scrollToElement(aboutMe)
+})
 
 links.forEach(li => {
   li.addEventListener('click',() => {
@@ -12,3 +18,10 @@ links.forEach(li => {
 burgerIcon.addEventListener('click', ()=>{
   menu.classList.toggle('closed-menu')
 })
+
+function scrollToElement(element) {
+  window.scrollTo({
+    behavior: 'smooth',
+    top: element.offsetTop
+  });
+}
