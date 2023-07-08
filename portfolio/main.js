91d86cd86774b2a1bce4c9b75c4e2309a97f4452
Chanda Gonet <chandagonet@gmail.com>
2023-07-08 08:45:46 -0400

    Revert

---
+++ portfolio/main.js
@@ -0,0 +1,33 @@
+import './style.css'
+
+const burgerIcon = document.getElementById('burger-icon')
+const menu = document.querySelector('nav')
+const links = document.querySelectorAll('li')
+const header = document.getElementById('header')
+const aboutMe = document.getElementById('about-me')
+
+header.addEventListener('click', () => {
+  scrollToElement(aboutMe)
+})
+
+links.forEach(li => {
+  li.addEventListener('click',() => {
+    menu.classList.toggle('closed-menu')
+
+  })
+})
+
+burgerIcon.addEventListener('click', ()=>{
+  menu.classList.toggle('closed-menu')
+})
+
+function scrollToElement(element) {
+  window.scrollTo({
+    behavior: 'smooth',
+    top: element.offsetTop
+  });
+}
+
+window.onload = () => {
+  console.log('Welcome, friend :)')
+}
